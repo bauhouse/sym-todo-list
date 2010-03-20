ec3522ec0728d32055c6a0a97c76f4f9b06a362f
Stephen Bau <bauhouse@gmail.com>
2010-03-20 14:16:09 -0700

    Add data sources for Items and Lists sections

---
+++ workspace/data-sources/data.lists.php
@@ -0,0 +1,63 @@
+<?php
+
+	require_once(TOOLKIT . '/class.datasource.php');
+	
+	Class datasourcelists extends Datasource{
+		
+		public $dsParamROOTELEMENT = 'lists';
+		public $dsParamORDER = 'desc';
+		public $dsParamLIMIT = '20';
+		public $dsParamREDIRECTONEMPTY = 'no';
+		public $dsParamSORT = 'system:id';
+		public $dsParamSTARTPAGE = '1';
+		public $dsParamASSOCIATEDENTRYCOUNTS = 'no';
+		public $dsParamINCLUDEDELEMENTS = array(
+				'name',
+				'items'
+		);
+
+		public function __construct(&$parent, $env=NULL, $process_params=true){
+			parent::__construct($parent, $env, $process_params);
+			$this->_dependencies = array();
+		}
+		
+		public function about(){
+			return array(
+					 'name' => 'Lists',
+					 'author' => array(
+							'name' => 'Stephen Bau',
+							'website' => 'http://home/sym/todolist',
+							'email' => 'bauhouse@gmail.com'),
+					 'version' => '1.0',
+					 'release-date' => '2010-03-20T20:52:45+00:00');	
+		}
+		
+		public function getSource(){
+			return '1';
+		}
+		
+		public function allowEditorToParse(){
+			return true;
+		}
+		
+		public function grab(&$param_pool=NULL){
+			$result = new XMLElement($this->dsParamROOTELEMENT);
+				
+			try{
+				include(TOOLKIT . '/data-sources/datasource.section.php');
+			}
+			catch(FrontendPageNotFoundException $e){
+				// Work around. This ensures the 404 page is displayed and
+				// is not picked up by the default catch() statement below
+				FrontendPageNotFoundExceptionHandler::render($e);
+			}			
+			catch(Exception $e){
+				$result->appendChild(new XMLElement('error', $e->getMessage()));
+				return $result;
+			}	
+
+			if($this->_force_empty_result) $result = $this->emptyXMLSet();
+			return $result;
+		}
+	}
+
