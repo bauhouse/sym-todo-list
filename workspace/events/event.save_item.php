b611ef76a0cbb94719ff747ddb6080342c9f6305
Stephen Bau <bauhouse@gmail.com>
2010-03-20 22:54:03 -0700

    Add Save Item and Save Items events (single and multiple entry events)

---
+++ workspace/events/event.save_item.php
@@ -0,0 +1,77 @@
+<?php
+
+	require_once(TOOLKIT . '/class.event.php');
+	
+	Class eventsave_item extends Event{
+		
+		const ROOTELEMENT = 'save-item';
+		
+		public $eParamFILTERS = array(
+			
+		);
+			
+		public static function about(){
+			return array(
+					 'name' => 'Save Item',
+					 'author' => array(
+							'name' => 'Stephen Bau',
+							'website' => 'http://home/sym/todolist',
+							'email' => 'bauhouse@gmail.com'),
+					 'version' => '1.0',
+					 'release-date' => '2010-03-21T04:30:10+00:00',
+					 'trigger-condition' => 'action[save-item]');	
+		}
+
+		public static function getSource(){
+			return '2';
+		}
+
+		public static function allowEditorToParse(){
+			return true;
+		}
+
+		public static function documentation(){
+			return '
+        <h3>Success and Failure XML Examples</h3>
+        <p>When saved successfully, the following XML will be returned:</p>
+        <pre class="XML"><code>&lt;save-item result="success" type="create | edit">
+  &lt;message>Entry [created | edited] successfully.&lt;/message>
+&lt;/save-item></code></pre>
+        <p>When an error occurs during saving, due to either missing or invalid fields, the following XML will be returned:</p>
+        <pre class="XML"><code>&lt;save-item result="error">
+  &lt;message>Entry encountered errors when saving.&lt;/message>
+  &lt;field-name type="invalid | missing" />
+  ...
+&lt;/save-item></code></pre>
+        <h3>Example Front-end Form Markup</h3>
+        <p>This is an example of the form markup you can use on your frontend:</p>
+        <pre class="XML"><code>&lt;form method="post" action="" enctype="multipart/form-data">
+  &lt;input name="MAX_FILE_SIZE" type="hidden" value="5242880" />
+  &lt;label>To Do
+    &lt;input name="fields[to-do]" type="text" />
+  &lt;/label>
+  &lt;label>Open
+    &lt;input name="fields[open]" checked="checked" type="checkbox" />
+  &lt;/label>
+  &lt;label>Order
+    &lt;input name="fields[order]" type="text" />
+  &lt;/label>
+  &lt;input name="action[save-item]" type="submit" value="Submit" />
+&lt;/form></code></pre>
+        <p>To edit an existing entry, include the entry ID value of the entry in the form. This is best as a hidden field like so:</p>
+        <pre class="XML"><code>&lt;input name="id" type="hidden" value="23" /></code></pre>
+        <p>To redirect to a different location upon a successful save, include the redirect location in the form. This is best as a hidden field like so, where the value is the URL to redirect to:</p>
+        <pre class="XML"><code>&lt;input name="redirect" type="hidden" value="http://home/sym/todolist/success/" /></code></pre>';
+		}
+		
+		public function load(){			
+			if(isset($_POST['action']['save-item'])) return $this->__trigger();
+		}
+		
+		protected function __trigger(){
+			include(TOOLKIT . '/events/event.section.php');
+			return $result;
+		}		
+
+	}
+
